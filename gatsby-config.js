59facca6105ddc7e108750bbf81eef2ea4da3b11
Corey Ward <corey.atx@gmail.com>
2020-03-16 20:11:35 -0500

    Use ENV to configure showing drafts

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,9 @@ module.exports = {
         projectId: process.env.SANITY_PROJECT_ID,
         dataset: process.env.SANITY_DATASET,
         token: process.env.SANITY_TOKEN,
-        overlayDrafts: process.env.NODE_ENV !== "production",
+        overlayDrafts:
+          process.env.NODE_ENV !== "production" &&
+          process.env.SHOW_DRAFTS !== "false",
         watchMode: process.env.NODE_ENV !== "production",
       },
     },
