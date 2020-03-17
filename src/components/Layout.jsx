56a42c834f1f7b7151cae8c961bf86df758bacb8
Corey Ward <corey.atx@gmail.com>
2020-03-16 23:47:11 -0500

    Add opengraph image

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,14 +4,29 @@ import { Global } from "@emotion/core"
 import Helmet from "react-helmet"
 import { globalStyles } from "styles/theme"
 
-const Layout = ({ title, children, className }) => (
-  <>
-    <Global styles={globalStyles} />
-    <Helmet title={`${title ? `${title} - ` : ""}Austin Takeout Tracker`} />
+const Layout = ({ title, children, className }) => {
+  const metaTitle = `${title ? `${title} - ` : ""}Austin Takeout Tracker`
+  const metaDesc =
+    "This project aims to track restaurants that are open during the covid-19 health crisis."
+  return (
+    <>
+      <Global styles={globalStyles} />
+      <Helmet title={metaTitle}>
+        <meta property="og:title" content={metaTitle} />
+        <meta property="og:description" content={metaDesc} />
+        <meta name="description" content={metaDesc} />
+        <meta
+          property="og:image"
+          content="https://www.takeouttracker.com/opengraph.png"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Helmet>
 
-    <main className={className}>{children}</main>
-  </>
-)
+      <main className={className}>{children}</main>
+    </>
+  )
+}
 
 export default Layout
 
