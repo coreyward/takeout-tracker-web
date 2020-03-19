894ddae080bf3b57dd1577f3fe0255d77a15891d
jbrennan <jon@safetyjon.com>
2020-03-19 17:40:56 -0500

    adds controlledIcon prop to IconRow component

--- src/components/IconRow.jsx
+++ src/components/IconRow.jsx
@@ -2,9 +2,11 @@ import React from "react"
 import PropTypes from "prop-types"
 import theme from "styles/theme"
 
-const IconRow = ({ icon: Icon, children, className }) => (
+const IconRow = ({ icon: Icon, controlledIcon, children, className }) => (
   <div css={{ display: "flex", marginBottom: 8 }} className={className}>
-    <Icon css={{ color: theme.n50, marginRight: 8, flex: "0 0 16px" }} />
+    {controlledIcon || (
+      <Icon css={{ color: theme.n50, marginRight: 8, flex: "0 0 16px" }} />
+    )}
     <div>{children}</div>
   </div>
 )
@@ -13,6 +15,7 @@ export default IconRow
 
 IconRow.propTypes = {
   icon: PropTypes.func.isRequired,
+  controlledIcon: PropTypes.object,
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
 }
