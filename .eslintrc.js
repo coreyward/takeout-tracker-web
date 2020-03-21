2b96627b76ab581a1fd1c646bd9c7a95f8d02033
Corey Ward <corey.atx@gmail.com>
2020-03-21 00:10:44 -0500

    Configure Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,15 @@ module.exports = {
   extends: [
     "standard",
     "plugin:react/recommended",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
   plugins: ["standard", "react", "react-hooks", "prettier"],
   rules: {
     "react/prop-types": 1,
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
-  }
-};
+    "react-hooks/exhaustive-deps": "warn",
+  },
+  env: {
+    jest: true,
+  },
+}
