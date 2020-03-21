d9e25108c45f68172b295a126ab33bec72c90b94
Corey Ward <corey.atx@gmail.com>
2020-03-21 13:38:54 -0500

    Add custom hours-parsing/matching routine

---
+++ src/lib/parseHours.js
@@ -0,0 +1,85 @@
+// Order reflects matcher precedence
+const daysOfWeek = [
+  { dow: 4, fragments: ["thursday", "thurs", "thur", "thu", "th"] },
+  { dow: 6, fragments: ["saturday", "sat", "sa"] },
+  { dow: 1, fragments: ["monday", "mon"] },
+  { dow: 2, fragments: ["tuesday", "tues", "tue"] },
+  { dow: 3, fragments: ["wednesday", "wed", "w"] },
+  { dow: 5, fragments: ["friday", "fri", "f"] },
+  { dow: 0, fragments: ["sunday", "sun"] },
+]
+
+export const parseTimes = times =>
+  times
+    .toLowerCase()
+    .replace("noon", "12pm")
+    .match(/[0-9]{1,2}(:[0-9]{2})?( *(a|p)m?)?/g)
+    .map(timeStr => {
+      let hr
+      let min = 0
+
+      if (timeStr.includes(":")) {
+        const [, minutes] = timeStr.split(":")
+        min = parseInt(minutes, 10)
+      }
+
+      // Account for 12–1 (am or pm)
+      hr = parseInt(timeStr, 10) === 12 ? 0 : parseInt(timeStr, 10) * 100
+
+      // Account for 12-hour time
+      if (timeStr.includes("p")) hr = hr + 1200
+
+      return hr + min
+    })
+
+const parseHours = hours => {
+  if (!/[0-9]/.test(hours)) return []
+  hours = hours.toLowerCase()
+
+  let days = daysOfWeek
+    .reduce((results, { fragments, dow }) => {
+      const frag = fragments.find(frag => hours.includes(frag))
+      if (frag) {
+        const index = hours.indexOf(frag)
+        results.push({ dow, index })
+      }
+      return results
+    }, [])
+    .sort((a, b) => a.index - b.index)
+    .map(({ dow }) => dow)
+
+  // If no single day was found, assume all week
+  if (days.length === 0) days = [0, 6]
+
+  const [startTime, endTime] = parseTimes(hours)
+
+  const [startDay, endDay = days[0]] = days
+
+  let res = []
+  const rangeEnd = endDay < startDay ? endDay + 7 : endDay
+  for (let i = startDay; i <= rangeEnd; i++) {
+    if (startTime > endTime) {
+      // crosses midnight
+      res.push({ weekday: i % 7, startTime, endTime: 2400 })
+      res.push({ weekday: (i + 1) % 7, startTime: 0, endTime })
+    } else {
+      res.push({ weekday: i % 7, startTime, endTime })
+    }
+  }
+  return res.sort((a, b) => a.weekday - b.weekday)
+}
+
+// Check if the provided hours include the provided target date
+export const hoursCover = (hours, targetDate = new Date()) => {
+  const openTimes = hours.map(hrs => parseHours(hrs)).flat()
+  const targetTime = targetDate.getHours() * 100 + targetDate.getMinutes()
+
+  return openTimes.some(
+    entry =>
+      entry.weekday === targetDate.getDay() &&
+      entry.startTime <= targetTime &&
+      (typeof entry.endTime === "undefined" || entry.endTime >= targetTime)
+  )
+}
+
+export default parseHours
---
+++ src/lib/parseHours.test.js
@@ -0,0 +1,173 @@
+import parseHours, { parseTimes, hoursCover } from "./parseHours"
+
+describe("parseTimes", () => {
+  test("single time, no minutes", () => {
+    expect(parseTimes("11am")).toEqual([1100])
+  })
+
+  test("single time, with minutes", () => {
+    expect(parseTimes("11:20am")).toEqual([1120])
+  })
+
+  test("implicit “am”", () => {
+    expect(parseTimes("11:30")).toEqual([1130])
+  })
+
+  test("12-hour time with “pm”", () => {
+    expect(parseTimes("11:30pm")).toEqual([2330])
+  })
+
+  test("multiple times", () => {
+    expect(parseTimes("11am–3pm")).toEqual([1100, 1500])
+    expect(parseTimes("11:20am–4:15pm")).toEqual([1120, 1615])
+  })
+
+  test("with spaces before am/pm", () => {
+    expect(parseTimes("Thu-Sun: 12 PM - 6 PM")).toEqual([1200, 1800])
+  })
+
+  test("with single-char am/pm", () => {
+    expect(parseTimes("Thu-Sun: 11a-4p")).toEqual([1100, 1600])
+  })
+
+  test("12:01am === 0001", () => {
+    expect(parseTimes("12:01am")).toEqual([1])
+  })
+
+  test("12:01pm === 1201", () => {
+    expect(parseTimes("12:01pm")).toEqual([1201])
+  })
+
+  test("Handle “noon”", () => {
+    expect(parseTimes("Noon to 3:33pm")).toEqual([1200, 1533])
+  })
+})
+
+describe("parseHours", () => {
+  test("single day default", () => {
+    expect(parseHours("Mon: 11am–8pm")).toEqual([
+      { weekday: 1, startTime: 1100, endTime: 2000 },
+    ])
+  })
+
+  test("single day, sloppy", () => {
+    expect(parseHours("th- 6a to 8:33pm")).toEqual([
+      { weekday: 4, startTime: 600, endTime: 2033 },
+    ])
+  })
+
+  test("times crossing midnight", () => {
+    expect(parseHours("friday 10pm–2am")).toEqual([
+      { weekday: 5, startTime: 2200, endTime: 2400 },
+      { weekday: 6, startTime: 0, endTime: 200 },
+    ])
+  })
+
+  test("two day span", () => {
+    expect(parseHours("Wed–Thurs: 5:55am–7pm")).toEqual([
+      { weekday: 3, startTime: 555, endTime: 1900 },
+      { weekday: 4, startTime: 555, endTime: 1900 },
+    ])
+  })
+
+  test("two-day spans crossing midnight", () => {
+    expect(parseHours("fri–sat 10pm–2am")).toEqual([
+      { weekday: 0, startTime: 0, endTime: 200 },
+      { weekday: 5, startTime: 2200, endTime: 2400 },
+      { weekday: 6, startTime: 0, endTime: 200 },
+      { weekday: 6, startTime: 2200, endTime: 2400 },
+    ])
+  })
+
+  test("three day span, non-wrapping", () => {
+    expect(parseHours("Wed–Fri: 5:55am–7pm")).toEqual([
+      { weekday: 3, startTime: 555, endTime: 1900 },
+      { weekday: 4, startTime: 555, endTime: 1900 },
+      { weekday: 5, startTime: 555, endTime: 1900 },
+    ])
+  })
+
+  test("three day span, weekend wrapping", () => {
+    expect(parseHours("Fri–Sun: 4pm–5:30pm")).toEqual([
+      { weekday: 0, startTime: 1600, endTime: 1730 },
+      { weekday: 5, startTime: 1600, endTime: 1730 },
+      { weekday: 6, startTime: 1600, endTime: 1730 },
+    ])
+  })
+
+  test("multi-day span with label", () => {
+    expect(parseHours("Dinner: Mon-Sun: 5pm–10pm")).toEqual([
+      { weekday: 0, startTime: 1700, endTime: 2200 },
+      { weekday: 1, startTime: 1700, endTime: 2200 },
+      { weekday: 2, startTime: 1700, endTime: 2200 },
+      { weekday: 3, startTime: 1700, endTime: 2200 },
+      { weekday: 4, startTime: 1700, endTime: 2200 },
+      { weekday: 5, startTime: 1700, endTime: 2200 },
+      { weekday: 6, startTime: 1700, endTime: 2200 },
+    ])
+  })
+
+  test("“everyday” with suffix", () => {
+    expect(parseHours("Everyday 7am - 10pm at all locations")).toEqual([
+      { weekday: 0, startTime: 700, endTime: 2200 },
+      { weekday: 1, startTime: 700, endTime: 2200 },
+      { weekday: 2, startTime: 700, endTime: 2200 },
+      { weekday: 3, startTime: 700, endTime: 2200 },
+      { weekday: 4, startTime: 700, endTime: 2200 },
+      { weekday: 5, startTime: 700, endTime: 2200 },
+      { weekday: 6, startTime: 700, endTime: 2200 },
+    ])
+  })
+
+  test("11am until sold out", () => {
+    expect(parseHours("11am until sold out")).toEqual([
+      { weekday: 0, startTime: 1100, endTime: undefined },
+      { weekday: 1, startTime: 1100, endTime: undefined },
+      { weekday: 2, startTime: 1100, endTime: undefined },
+      { weekday: 3, startTime: 1100, endTime: undefined },
+      { weekday: 4, startTime: 1100, endTime: undefined },
+      { weekday: 5, startTime: 1100, endTime: undefined },
+      { weekday: 6, startTime: 1100, endTime: undefined },
+    ])
+  })
+
+  test("garbage in, empty array out", () => {
+    expect(parseHours("Check website")).toEqual([])
+  })
+})
+
+describe("hoursCover", () => {
+  const tuesdayAt3pm = new Date(2020, 2, 17, 15, 0, 0)
+  const fridayAt10pm = new Date(2020, 2, 20, 22, 0, 0)
+  const saturdayAt1am = new Date(2020, 2, 21, 1, 0, 0)
+
+  test("positive for matching range", () => {
+    expect(hoursCover(["Tuesday 8am–8pm"], tuesdayAt3pm)).toBeTruthy()
+  })
+
+  test("negative for non-matching range", () => {
+    expect(hoursCover(["Tuesday 8pm–11pm"], tuesdayAt3pm)).toBeFalsy()
+  })
+
+  test("excludes other days of week", () => {
+    expect(hoursCover(["Wednesday 8am–8pm"], tuesdayAt3pm)).toBeFalsy()
+  })
+
+  test("implicit every-day", () => {
+    expect(hoursCover(["8am–8pm"], tuesdayAt3pm)).toBeTruthy()
+  })
+
+  test("range exclusion with implicit every-day", () => {
+    expect(hoursCover(["8am–8pm"], fridayAt10pm)).toBeFalsy()
+  })
+
+  test("time range crossing midnight matches", () => {
+    expect(hoursCover(["Mon 10pm–2am"], tuesdayAt3pm)).toBeFalsy()
+    expect(hoursCover(["Friday 10pm–2am"], fridayAt10pm)).toBeTruthy()
+    expect(hoursCover(["Friday 10pm–2am"], saturdayAt1am)).toBeTruthy()
+  })
+
+  test("negative for “closed”", () => {
+    expect(hoursCover(["Sat: CLOSED"], saturdayAt1am)).toBeFalsy()
+  })
+})
