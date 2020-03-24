f163fede6021828dc1c63909b36574ce528253e9
Corey Ward <corey.atx@gmail.com>
2020-03-23 19:54:34 -0500

    Hide closed restaurants on list pages by default

--- src/templates/List.jsx
+++ src/templates/List.jsx
@@ -26,6 +26,7 @@ const List = ({ data: { list } }) => {
       <RestaurantsViewer
         title={list.name}
         restaurants={list.restaurants}
+        defaultFilters={["hideClosed"]}
         defaultViewMode={MODES.TILE}
         showingAll={false}
       />
